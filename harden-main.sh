080b182dd54391bc7e77ecc29266cc2bfcd7ca8d
nano9911 <aalkhaldi8@gmail.com>
2022-04-03 00:39:01 +0300

    Added error checking, and some fixes

--- harden-main.sh
+++ harden-main.sh
@@ -6,8 +6,16 @@ OPTION1=$2
 
 MAIN_DIR="/usr/share/harden"
 SCRIPTS_DIR="$MAIN_DIR/scripts/"
+
+STATUS_DIR="$MAIN_DIR/status"
+[[ -e $STATUS_DIR ]] && mkdir $STATUS_DIR
+
 MESSAGES_DIR="$MAIN_DIR/messages/"
+[[ -e $MESSAGES_DIR ]] && mkdir $MESSAGES_DIR
+
 ACTIONS_DIR="$MAIN_DIR/actions"
+[[ -e $ACTIONS_DIR ]] && mkdir $ACTIONS_DIR
+
 CONFIG_FILE="/etc/harden/harden.conf"
 DEFAULT_PROFILE_FILE="/etc/harden/default.profile"
 PROFILE_FILE="/etc/harden/admin-choice.profile"
@@ -23,16 +31,16 @@ ACTIONS_FILE="$ACTIONS_DIR/$(date +%F_%H:%M:%S).sh"
 # Uncomment this line if you chose to use journald for logging
 # (by setting the StandardOutput & StandardError variables
 # in the service unit file to "journal")
-# exec 1>>$LOG_FILE 2>>$LOG_FILE
+# exec 1>>$LOG_FILE 2>&1
 
 echo "Harden service is starting up ..."
 
 harden-run()   {
     local CURRENT_PROFILE_FILE=$1
 
-    # Create Log and status file forthe current run.
+    # Create Log, status, messages and actions file forthe current run.
     #echo > $LOG_FILE
-    echo > $STATUS_FILE
+    echo > $STATUS_FILE > $MESSAGES_FILE > $ACTIONS_FILE
 
     # Run tail command in follow mode in the background, so we can
     # get the data from the status file in stdout automatically.
@@ -60,21 +68,25 @@ harden-run()   {
     echo "# Created at $(date)" >> $ACTIONS_FILE
 
     cat $CURRENT_PROFILE_FILE | while read line; do
-        script=$(echo $line | awk '{print $3;}')
+        rule=$(echo $line | awk '{print $1;}')
+        script=$(echo $line | awk '{print $2;}')
         bash $script $STATUS_FILE $MESSAGES_FILE $ACTIONS_FILE
     done
 
     echo "Harden service has finished"
+
+    return 0
 }
 
 take-action()   {
     echo "Taking Actions from file " $()
-    bash /usr/share/harden/last-actions
+    bash "$MAIN_DIR/last-actions"
+    return $?
 }
 
 show-messages() {
     local MESSAGES_DATE=$1
-    [[ $(ls "$MESSAGES_DIR/$MESSAGES_DATE*" | wc -w) == 0 ]] && echo "No messages found for this date" && return 1
+    [[ $(ls "$MESSAGES_DIR/$MESSAGES_DATE*" | wc -w) == 0 ]] && echo "No messages found for this date ($MESSAGES_DATE)" && return 1
 
     for i in "$MESSAGES_DIR/$MESSAGES_DATE*"
     do
@@ -86,7 +98,7 @@ show-messages() {
 
 show-actions()  {
     local ACTIONS_DATE=$1
-    [[ $(ls "$ACTIONS_DIR/$ACTIONS_DATE*" | wc -w) == 0 ]] && echo "No actions found for this date" && return 1
+    [[ $(ls "$ACTIONS_DIR/$ACTIONS_DATE*" | wc -w) == 0 ]] && echo "No actions found for this date ($ACTIONS_DATE)" && return 1
 
     for i in "$ACTIONS_DIR/$ACTIONS_DATE*"
     do
@@ -96,9 +108,12 @@ show-actions()  {
     return 0
 }
 
+RETURN_VALUE=""
 # Check what mode we are running in
-[[ $OPERATE_MODE == "setup" ]] && harden-run $DEFAULT_PROFILE_FILE && return 1
-[[ $OPERATE_MODE == "scan" ]] && harden-run $PROFILE_FILE && return 1
-[[ $OPERATE_MODE == "act" ]] && take-action && return 1
-[[ $OPERATE_MODE == "messages" ]] && show-messages $OPTION1 && return 1
-[[ $OPERATE_MODE == "actions" ]] && show-actions $OPTION1 && return 1
+[[ $OPERATE_MODE == "setup" ]] && RETURN_VALUE=$(harden-run $DEFAULT_PROFILE_FILE)
+[[ $OPERATE_MODE == "scan" ]] && RETURN_VALUE=$(harden-run $PROFILE_FILE)
+[[ $OPERATE_MODE == "act" ]] && RETURN_VALUE=$(take-action)
+[[ $OPERATE_MODE == "messages" ]] && RETURN_VALUE=$(show-messages $OPTION1)
+[[ $OPERATE_MODE == "actions" ]] && RETURN_VALUE=$(show-actions $OPTION1)
+
+exit $RETURN_VALUE
