89f04c931cc4106ea2ee430e113e19eb27c1c060
nano9911 <aalkhaldi8@gmail.com>
2022-04-13 02:37:13 +0300

    added features to main

--- harden-main.sh
+++ harden-main.sh
@@ -13,27 +13,25 @@ shift
 # the case switch statement to test them
 while [[ $# -gt 0 ]]; do
     case $1 in
-        -md|--main-directory)
-            MAIN_DIR="$2"
+        -cf|--config-file)  # Use a configuration file from user choice
+            CONFIG_FILE=$2
+            shift 2
+            ;;
+        -mf|--messages-file)    # Use/Create a messages file from user choice
+            MESSAGES_FILE=$2
             shift 2 # shift the arguments 2 times (we used two arguments)
             ;;
-        -md=*|--main-directory=*)
-            MAIN_DIR="${2#*=}"
-            shift # shift the arguments once (we used a single argument)
+        -af|--actions-file) # Use/Create an actions file from user choice
+            ACTIONS_FILE=$2
+            shift 2
             ;;
-        -pf|--profile-file)
+        -pf|--profile-file) # Use a profile file from user choice
             PROFILE_FILE="$2"
             shift 2 
             ;;
-        -pf=*|--profile-file=*)
-            PROFILE_FILE="${2#*=}"
-            shift
-            ;;
-        -mf|--messages-file)
-            ;;
-        -af|--actions-file)
-            ;;
-        -d|--date)
+        -d|--date)  # This option is used in (list-messages, list-actions) operate modes
+            DATE_TO_LIST=$2
+            shift 2
             ;;
         -*|--*)
             echo "Unknown option $1"
@@ -51,26 +49,22 @@ set -- "${POSITIONAL_ARGS[@]}"
 
 MAIN_DIR="/usr/share/harden"    # Default Main Directory
 CONFIG_DIR="/etc/harden"    # Default Configuration Directory
-CONFIG_FILE="$CONFIG_DIR/harden.conf"   # Default Configuration File
 DEFAULT_PROFILE_FILE="$CONFIG_DIR/default.profile"  # Default Profile File
-PROFILE_FILE="$CONFIG_DIR/admin-choice.profile" # Default User Choice Profile File
 
 SCRIPTS_DIR="$MAIN_DIR/scripts" # Default Scripts Directory
 
 STATUS_DIR="$MAIN_DIR/status"   # Default Status Directory
-# Check if Directory exists, and if not then create it
-[[ ! -d $STATUS_DIR ]] && mkdir $STATUS_DIR
-STATUS_FILE="$STATUS_DIR/$RUNTIME_DATE.status"
-
+[[ ! -d $STATUS_DIR ]] && mkdir $STATUS_DIR # Check if Directory exists, and if not then create it
 MESSAGES_DIR="$MAIN_DIR/messages"   # Default Messages Directory
-# Check if Directory exists, and if not then create it
-[[ ! -d $MESSAGES_DIR ]] && mkdir $MESSAGES_DIR
-MESSAGES_FILE="$MESSAGES_DIR/$RUNTIME_DATE.message"
-
+[[ ! -d $MESSAGES_DIR ]] && mkdir $MESSAGES_DIR # Check if Directory exists, and if not then create it
 ACTIONS_DIR="$MAIN_DIR/actions" # Default Actions Directory
-# Check if Directory exists, and if not then create it
-[[ ! -d $ACTIONS_DIR ]] && mkdir $ACTIONS_DIR
-ACTIONS_FILE="$ACTIONS_DIR/$RUNTIME_DATE.sh"
+[[ ! -d $ACTIONS_DIR ]] && mkdir $ACTIONS_DIR   # Check if Directory exists, and if not then create it
+
+${CONFIG_FILE:="$CONFIG_DIR/harden.conf"}   # Default Configuration File
+${PROFILE_FILE:="$CONFIG_DIR/admin-choice.profile"} # Default User Choice Profile File
+${STATUS_FILE:="$STATUS_DIR/$RUNTIME_DATE.status"}  # Currently used status file
+${MESSAGES_FILE:="$MESSAGES_DIR/$RUNTIME_DATE.message"} # Currently used messages file
+${ACTIONS_FILE:="$ACTIONS_DIR/$RUNTIME_DATE.sh"}    # Currently used Actions file
 
 # Redirect stdout and stderr to the log file, so everything
 # will be recorded in it. Also, due to the tail command on the
@@ -84,7 +78,15 @@ ACTIONS_FILE="$ACTIONS_DIR/$RUNTIME_DATE.sh"
 #   echo > $LOG_FILE
 #	exec 1>>$LOG_FILE 2>&1
 
-echo "Harden service is starting up ..."
+# Print startup message with run time settings
+echo "\
+Harden service is starting up ...
+CONFIG_FILE = $CONFIG_FILE
+MAIN_DIR = $MAIN_DIR
+PROFILE_FILE = $PROFILE_FILE
+STATUS_FILE = $STATUS_FILE
+MESSAGES_FILE = $MESSAGES_FILE
+ACTIONS_FILE = $ACTIONS_FILE"
 
 harden-run()   {
     local CURRENT_PROFILE_FILE=$1
@@ -92,35 +94,35 @@ harden-run()   {
     # Create Log, status, messages and actions file for the current run.
     touch $STATUS_FILE $MESSAGES_FILE $ACTIONS_FILE
 
-    # Run tail command in follow mode in the background, so we can
-    # get the data from the status file in stdout automatically.
-    tail -f $STATUS_FILE &
-    # Set a trap condition for the tail command, so it will end,
-    # when the process (script) exits.
-    trap "pkill -P $$" EXIT
-
+    tail -f $STATUS_FILE &  # Run tail command in follow mode in the
+                            # background, so we can get the data from
+                            # the status file in stdout automatically.
+    trap "pkill -P $$" EXIT # Set a trap condition for the tail command,
+                            # so it will end, when the process (script) exits.
     tail -f $MESSAGES_FILE &
     trap "pkill -P $$" EXIT
-
     tail -f $ACTIONS_FILE &
     trap "pkill -P $$" EXIT
 
     # Create/relink a symlink to the last status file
-    [[ -e /usr/share/harden/last-status ]] && rm /usr/share/harden/last-status
-    ln -s $STATUS_FILE /usr/share/harden/last-status
-    [[ -e /usr/share/harden/last-messages ]] && rm /usr/share/harden/last-messages
-    ln -s $MESSAGES_FILE /usr/share/harden/last-messages
-    [[ -e /usr/share/harden/last-actions ]] && rm /usr/share/harden/last-actions
-    ln -s $ACTIONS_FILE /usr/share/harden/last-actions
+    [[ -e "$MAIN_DIR/last-status" ]] && rm "$MAIN_DIR/last-status"
+    [[ -e "$MAIN_DIR/last-messages" ]] && rm "$MAIN_DIR/last-messages"
+    [[ -e "$MAIN_DIR/last-actions" ]] && rm "$MAIN_DIR/last-actions"
+    ln -s $STATUS_FILE "$MAIN_DIR/last-status"
+    ln -s $MESSAGES_FILE "$MAIN_DIR/last-messages"
+    ln -s $ACTIONS_FILE "$MAIN_DIR/last-actions"
 
     cat $CURRENT_PROFILE_FILE | while read line; do
         rule=$(echo $line | awk '{print $1;}')
         script=$(echo $line | awk '{print $2;}')
-        bash $script $STATUS_FILE $MESSAGES_FILE $ACTIONS_FILE
+        if [[ ${script%/*} -eq $SCRIPTS_DIR ]] then
+            bash $script -sf $STATUS_FILE -mf $MESSAGES_FILE -af $ACTIONS_FILE
+        else
+            echo "Script $script does not exist not in the $SCRIPTS_DIR."
+        fi
     done
 
     echo "Harden service has finished"
-
     return 0
 }
 
@@ -131,26 +133,24 @@ take-action()   {
 }
 
 show-messages() {
-    local MESSAGES_DATE=$1
-    [[ $(ls "$MESSAGES_DIR/$MESSAGES_DATE*" | wc -w) == 0 ]] && echo "No messages found for this date ($MESSAGES_DATE)" && return 1
-
-    for i in "$MESSAGES_DIR/$MESSAGES_DATE*"
-    do
-        cat $i
-    done
-
-    return 0
+    if [[ $(ls "$MESSAGES_DIR/$DATE_TO_LIST*" | wc -w) == 0 ]] then
+        echo "No messages found for this date ($DATE_TO_LIST)"
+        return 1
+    else
+        for i in "$MESSAGES_DIR/$DATE_TO_LIST*"
+        do
+            cat $i
+        done
+        return 0
+    fi
 }
 
 show-actions()  {
-    local ACTIONS_DATE=$1
-    [[ $(ls "$ACTIONS_DIR/$ACTIONS_DATE*" | wc -w) == 0 ]] && echo "No actions found for this date ($ACTIONS_DATE)" && return 1
-
-    for i in "$ACTIONS_DIR/$ACTIONS_DATE*"
+    [[ $(ls "$ACTIONS_DIR/$DATE_TO_LIST*" | wc -w) == 0 ]] && echo "No actions found for this date ($DATE_TO_LIST)" && return 1
+    for i in "$ACTIONS_DIR/$DATE_TO_LIST*"
     do
         cat $i
     done
-
     return 0
 }
 
@@ -164,15 +164,14 @@ check-and-run()	{
         ;;
         take-actions)        RETURN_VALUE=$(take-action)
         ;;
-        list-messages)   RETURN_VALUE=$(show-messages $OPTION1)
+        list-messages)   RETURN_VALUE=$(show-messages)
         ;;
-        list-actions)    RETURN_VALUE=$(show-actions $OPTION1)
+        list-actions)    RETURN_VALUE=$(show-actions)
         ;;
         *)
             echo "Please specify one of the available modes (setup - scan - act - messages - actions)"
         ;;
     esac
-
 	return $RETURN_VALUE
 }
 
