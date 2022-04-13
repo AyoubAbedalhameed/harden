f8b8de933cc110f385cd056270b7a2fca832c222
nano9911 <aalkhaldi8@gmail.com>
2022-04-13 06:35:35 +0300

    edited parameters passed to scripts

--- scripts/kernel-hardin.sh
+++ scripts/kernel-hardin.sh
@@ -3,16 +3,44 @@
 # recommended solutions and tips
 
 CONFIG_FILE="/usr/share/harden/config/kernel-parametrs.conf"
-STATUS_FILE=$1
-MESSAGES_FILE=$2
-ACTIONS_FILE=$3
 KERNEL_ACTIONS_FILE="/usr/share/config/$(date +%F_%H:%M:%S)_kernel-actions.sh"
+
+# Loop through all command line arguments, and but them in
+# the case switch statement to test them
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -sf|--status-file)  # Use a configuration file from user choice
+            STATUS_FILE=$2
+            shift 2
+            ;;
+        -mf|--messages-file)    # Use/Create a messages file from user choice
+            MESSAGES_FILE=$2
+            shift 2 # shift the arguments 2 times (we used two arguments)
+            ;;
+        -af|--actions-file) # Use/Create an actions file from user choice
+            ACTIONS_FILE=$2
+            shift 2
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arguments
+            shift
+            ;;
+    esac
+done
+
+# Restore Positional Arguments (those which has not been used)
+set -- "${POSITIONAL_ARGS[@]}"
 
 cat $CONFIG_FILE | while read line; do
 	key=`echo $line | awk '{print $1;}'`
 	value=`echo $line | awk '{print $2}'`
 	value=${value//,/$'\t'}
 	message=${line##*message=}
+	message=${line%action=*}
 	action=${line##*action=}
 
 	current=$(sysctl -en $key)
