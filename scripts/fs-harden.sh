cd3bad47d80216735ddd7a2a0402d5b34b61f1a1
nano9911 <aalkhaldi8@gmail.com>
2022-05-16 06:01:14 +0300

    options 99% ready, and scan script ready for /etc/fstab

--- scripts/fs-harden.sh
+++ scripts/fs-harden.sh
@@ -54,19 +54,55 @@ STATUS_FILE=${STATUS_FILE:="$MAIN_DIR/status/$RUNTIME_DATE.status"}	# Currently 
 MESSAGES_FILE=${MESSAGES_FILE:="$MAIN_DIR/messages/$RUNTIME_DATE.message"}	# Currently used messages file
 ACTIONS_FILE=${ACTIONS_FILE:="$MAIN_DIR/actions/$RUNTIME_DATE.sh"}	# Currently used Actions file
 
+FS_ACTIONS_FILE="$MAIN_DIR/scripts/fs-actions_$RUNTIME_DATE.sh"
+source $MAIN_DIR/resources/fs-options.rc
+
 # Queue the requested value from the JSON profile file by jq
 PROFILE=$(jq '.[] | select(.name=="fs")' $PROFILE_FILE)	# Save our object from the array
 check-pf()  {   return $(echo $PROFILE | jq ".fs.$1");  }
 
-source $MAIN_DIR/resources/fs-options.rc
+CURRENT_MOUNT_OPTIONS=()
+FSTAB_MOUNT_OPTIONS=()
 
-check-fstab() {
-  FS_FILE="/etc/fstab"
+# Get mount information from /etc/fstab
+cat /etc/fstab | while read line; do
+	[[ $line =~ "#" ]] && continue
+	L_DEVICE=$(echo line | awk '{print $1;}')
+	L_MOUNT_POINT=$(echo line | awk '{print $2;}')
+	L_FS_TYPE=$(echo line | awk '{print $3;}')
+
+	L_MOUNT_OPTIONS=$(echo line | awk '{print $4;}')
+	L_MOUNT_OPTIONS=${L_MOUNT_OPTIONS/,/ /}
+
+	# Check if the mount point in /etc/fstab exists in our list of covered mount points
+	if [[ -n MOUNT_POINTS[$L_MOUNT_POINT] ]] then
+		# Check if file system type is the one recommended
+		if [[ $L_FS_TYPE -eq $(echo ${MOUNT_POINTS[$L_MOUNT_POINT]} | awk '{print $1;}') ]] then
+			echo "FileSystem-Hardening($L_MOUNT_POINT) $ 0" >> $STATUS_FILE
+		fi
+
+		# Loop through the mount options of the mount point and check if they are the suitable recommended ones
+		for opt in "${MOUNT_OPTIONS[@]}"; do
+			[[ $L_MOUNT_OPTIONS =~ (^|[[:space:]])"$opt"($|[[:space:]]) ]]  && continue
+
+			echo "FileSystem-Hardening($L_MOUNT_POINT) $opt 0" >> $STATUS_FILE
+			echo "FileSystem-Hardening[$L_MOUNT_POINT]: Mount option $opt is missing from /etc/fstab for $L_MOUNT_POINT mount point, it's recommended to be used.
+${MOUNT_OPTIONS[$opt]}" >> $MESSAGES_FILE
+		done
+	fi
+done
+
+# Get current running mount information from /proc/mounts
+cat /proc/mounts | while read line; do
+	
+done
+
+write-hidepid()	{
+	SYSTEMD_LOGIND_HIDEPID_FILE="/etc/systemd/system/systemd-logind.service.d/hidepid.conf"
+	[[ ! -f $SYSTEMD_LOGIND_HIDEPID_FILE ]] && touch $SYSTEMD_LOGIND_HIDEPID_FILE
+	echo "[Service]" >> $SYSTEMD_LOGIND_HIDEPID_FILE
+	echo "SupplementaryGroups=proc" >> $SYSTEMD_LOGIND_HIDEPID_FILE
 }
 
-check-proc-mount() {
-  FS_FILE="/proc/mounts"
-}
 
-check-fstab
-check-proc-mounts
+echo "$FS_ACTIONS_FILE" >> $ACTIONS_FILE
