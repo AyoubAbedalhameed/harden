d5881eec47956f582dbebe53a341cd1c20faaf6b
Ayoub Abed Alhameed <aasedqiabedalhameed173@cit.just.edu.jo>
2022-05-27 22:53:53 +0300

    Update firewall-harden.sh
    (General Fixes) 
    
    This file has been erased and overwritten (Copy/Paste) using the GitHub web interface.

--- scripts/firewall-harden.sh
+++ scripts/firewall-harden.sh
@@ -1,18 +1,14 @@
+
 #!/usr/bin/env bash
 
 #Prevent overwriting files
 set -C
 
-#Importing firewall rules: 
-source iptables-rules.rc 
-
-
-
 #Fetching Script run time. 
 RUNTIME_DATE=$(date +%F_%H:%M:%S)	# Runtime date and time
 
 
-echo "$0:$RUNTIME_DATE   script is running .. "
+echo "$SCRIPT_NAME:$RUNTIME_DATE   script is running .. "
 
 #Getting User's Options:
 # Loop through all command line arguments, and but them in
@@ -65,19 +61,20 @@ set -- "${POSITIONAL_ARGS[@]}"
 
 # Preparing not initialized Filesystem Variables :
 MAIN_DIR=${MAIN_DIR:="/usr/share/harden"}
-PROFILE_FILE=${PROFILE_FILE:="/etc/harden/default.profile"}	# Use Default User Choice Profile File, 
+PROFILE_FILE=${PROFILE_FILE:="/etc/harden/profile-file.json"}	# Use Default User Choice Profile File, 
 								# if not set by a positional parameter (command line argument)
 STATUS_FILE=${STATUS_FILE:="$MAIN_DIR/status/$RUNTIME_DATE.status"}	# Currently used status file
 MESSAGES_FILE=${MESSAGES_FILE:="$MAIN_DIR/messages/$RUNTIME_DATE.message"}	# Currently used messages file
 ACTIONS_FILE=${ACTIONS_FILE:="$MAIN_DIR/actions/$RUNTIME_DATE.sh"}	# Currently used Actions file
+SCRIPT_NAME=`basename $0`
 
 SOURCE_FILE="$MAIN_DIR/resources/iptables-rules.rc"
-FIREWALL_ACTIONS_FILE="$MAIN_DIR/config/$RUNTIME_DATE-firewall-actions.sh"
  
 # Preparing not initialized other Variables:
 DEBUG=${DEBUG:=0}
 
-
+#Importing firewall rules: 
+source "$MAIN_DIR/resources/iptables-rules.rc"
 
 
 
@@ -88,16 +85,16 @@ PROFILE=$(jq '.[] | select(.name=="firewall")' $PROFILE_FILE)	# Save our object 
 
 #Cheking the Acceptance of firewall-hardening Checks: 
 if [[ `echo $PROFILE | jq '.firewall.check' ` -ne 1 ]] ; then 
-echo "$RUNTIME_DATE:$0:Terminates, Checking is now allowed"
+echo "$RUNTIME_DATE:$SCRIPT_NAME:Terminates, Checking is now allowed"
 exit
 fi
 
 
 
+GENERAL_ACTIONS_ACCEPTENCE=$( echo $PROFILE | jq '.firewall.action' )
 
 #Cheking the Acceptance of firewall-hardening Actions: 
-if [[ `echo $PROFILE | jq '.firewall.action'` -ne 1 ]] ; then 
-echo "#!/usr/bin/env bash" >> $FIREWALL_ACTIONS_FILE ; fi
+[[ $GENERAL_ACTIONS_ACCEPTENCE -eq 1 ]] && echo -e "#!/usr/bin/env bash" >> $ACTIONS_FILE
 
 
 
@@ -110,16 +107,16 @@ check-pf()  { return $(echo $PROFILE | jq ".firewall.$1.$2");  }
 ## ReplaceParameters() function, used to add the user parameters to the iptables rule.
  ReplaceParameters () 
 { #Usage: ReplaceParameter <RulesCount> <RuleName> <ParametersList> 
-
+													
 
 	for (( i=1; i<=$1 ; i++ )) ; do 	  #
-  
-    	local Temp="${FW_Rules["$2,$i"]}" #Fetching the current tule from the rules dictionary. 
+	
+    	local Temp="${FW_Rules["$2,$i"]}" #Fetching the current Rule from the rules dictionary. 
     	[[ "$3" == "null" ]]  && echo "$Temp\n" && continue ; #If the parameters list is null, replace nothing.
     	local PARAMETERS_COUNTER=0		 	 #Used as index do define the current parameter number in the loop.
    		 for PARAMETER in $3 ; do
-
-        	Temp=$( echo $Temp | sed "s/@${PARAMETERS_COUNTER}/${PARAMETER}/g" )  #Replacing all occurrences for the current parameter in the current string. 
+														
+        	Temp=$( echo $Temp | sed "s/@${PARAMETERS_COUNTER}/${PARAMETER//'/'/\\/}/g" )  #Replacing all occurrences for the current parameter in the current string. 
         	PARAMETERS_COUNTER=$( expr $PARAMETERS_COUNTER + 1 ) 				  #increment the param index. 		
 
     	done
@@ -149,63 +146,132 @@ check_rule()
 }
 
 
-#Cheking Firewall Services: 
+#write_action Function, used to echo the required commands to apply rules. 
+write_action(){
 
-Flag=1 
-
-
-#Checking Firewalld Service:  
-echo "$RUNTIME_DATE:$0:Checking firewalld service status" 
-systemctl status firewalld
-firewalld_status=$?
-
-if [[ ! $firewalld_status -eq  4 ]] ; then 
-firewalld_enabled=$(systemctl is-enabled firewalld) 
-firewalld_active=$(systemctl is-active firewalld) ; Flag=$? 
-firewalld_installed=1
-else firewalld_enabled="disabled" ; firewalld_active="inactive" ; firewalld_installed=0; fi 
-
-
-#Cheking Iptables Service:  
-echo "$RUNTIME_DATE:$0:Checking firewalld service status" 
-systemctl status iptables 
-iptables_status=$? 
-
-if [[ ! $iptables_status -eq  4 ]] ; then 
-iptables_enabled=$(systemctl is-enabled iptables) ; echo $iptables_enabled
-iptables_active=$(systemctl is-active iptables); Flag=$?
-iptables_installed=1
-else iptables_enabled="disabled" ; iptables_active="inactive" ; iptables_installed=0 ; fi 
-
-
-echo "$0.firewalld.installed $firewalld_installed" >> $STATUS_FILE
-echo "$0.firewalld.enabled $firewalld_enabled" >> $STATUS_FILE 
-echo "$0.firewalld.active $firewalld_active " >> $STATUS_FILE
-
-echo "$0.iptables.installed $iptables_installed" >> $STATUS_FILE
-echo "$0.iptables.enabled $iptables_enabled" >> $STATUS_FILE
-echo "$0.iptabled.active $iptables_active" >> $STATUS_FILE
+	local RULES=$(</dev/stdin)
+	                    				[[ $DEBUG -eq 1 ]] && echo -e "Local Rules are ($RULES)"
+										[[ $DEBUG -eq 1 ]] && echo "write_action is running"
+	
+	while read RULE ; do
+    	               					 [[ $DEBUG -eq 1 ]] && echo "The rule is $RULE"
+    	echo -e "iptables -A $RULE" >> $ACTIONS_FILE
+    done <<< "$RULES"
+    
+	}
 
 
 
+	#Cheking Firewall Services: 
+	CHECK_FIREWALL_SERVICES(){
 
-if [[ iptables_installed -eq 0 && firewalld_installed -eq 0 ]] 
-then echo "$0:$RUNTIME_DATE No Firewall Service Installed on this machine, at least one firewall service should be running" >> $MESSAGES_FILE
-NoFireWall=0 ; fi
-
-
-[[ $Flag -ne 0 ]] && echo "$0:$RUNTIME_DATE Firewall services are not enabled on this machine, you should enable one firewall service at least on your system" >> $MESSAGES_FILE 
+	local Flag=1 
+	IPTABLES_NEW_INSTALLATION=0
 
 
-if [[ iptables_installed -ne 1 ]] ; then 
+	#Checking Firewalld Service:  
+	echo "$RUNTIME_DATE:$SCRIPT_NAME:Checking firewalld service status" 
+	systemctl status firewalld
+	firewalld_status=$?
 
-    echo "$0:$RUNTIME_DATE: iptables is not installed, iptables rules checking will be skipped"
-    exit
+	if [[ ! $firewalld_status -eq  4 ]] ; then 
+	firewalld_enabled=$(systemctl is-enabled firewalld)
+	firewalld_active=$(systemctl is-active firewalld) ; Flag=$? 
+	firewalld_installed=1
+	else firewalld_enabled="disabled" ; firewalld_active="inactive" ; firewalld_installed=0; fi 
+
+	[[ ($GENERAL_ACTIONS_ACCEPTENCE -eq 1) && ($firewalld_enabled == "enabled") ]] && echo "systemctl disable firewalld" >> $ACTIONS_FILE
+	[[ ($GENERAL_ACTIONS_ACCEPTENCE -eq 1) && ($firewalld_active -eq 1) ]] && echo "systemctl stop firewalld" >> $ACTIONS_FILE
+
+	#Cheking Iptables Service:  
+	echo "$RUNTIME_DATE:$SCRIPT_NAME:Checking firewalld service status" 
+	systemctl status iptables 
+	iptables_status=$? 
+
+	if [[ ! $iptables_status -eq  4 ]] ; then 
+	iptables_enabled=$(systemctl is-enabled iptables)  || echo "systemctl enable iptables" >> $ACTIONS_FILE
+	iptables_active=$(systemctl is-active iptables); Flag=$? ; [[ $Flag -ne 0 ]] && echo "systemctl start iptables" >> $ACTIONS_FILE
+	iptables_installed=1
+	else iptables_enabled="disabled" ; iptables_active="inactive" ; iptables_installed=0 ; fi 
+
+
+	echo "$SCRIPT_NAME.firewalld.installed $firewalld_installed" >> $STATUS_FILE
+	echo "$SCRIPT_NAME.firewalld.enabled $firewalld_enabled" >> $STATUS_FILE 
+	echo "$SCRIPT_NAME.firewalld.active $firewalld_active " >> $STATUS_FILE
+
+	echo "$SCRIPT_NAME.iptables.installed $iptables_installed" >> $STATUS_FILE
+	echo "$SCRIPT_NAME.iptables.enabled $iptables_enabled" >> $STATUS_FILE
+	echo "$SCRIPT_NAME.iptabled.active $iptables_active" >> $STATUS_FILE
+
+
+
+
+	if [[ iptables_installed -eq 0 && firewalld_installed -eq 0 ]] 
+	then echo "$SCRIPT_NAME:$RUNTIME_DATE No Firewall Service Installed on this machine, at least one firewall service should be running" >> $MESSAGES_FILE
+	NoFireWall=0 ; fi
+
+
+	[[ $Flag -ne 0 ]] && echo "$SCRIPT_NAME:$RUNTIME_DATE Firewall services are not enabled on this machine, you should enable one firewall service at least on your system" >> $MESSAGES_FILE 
+}
+
+
+
+
+
+
+
+CHECK_FIREWALL_SERVICES
+
+#Intsalling iptables if it is not installed: 
+if [[ (iptables_installed -ne 1) && (GENERAL_ACTIONS_ACCEPTENCE -eq 1) ]] ; then 
+									[[ $DEBUG -eq 1 ]] && echo "$SCRIPT_NAME:$RUNTIME_DATE: Installing iptables .. "
+	yum -y install iptables && IPTABLES_NEW_INSTALLATION=1
+	yum list installed  | grep "iptables-services" && iptables_installed=1 && echo "$SCRIPT_NAME:$RUNTIME_DATE: iptables is installed succesfully" >> $MESSAGES_FILE
+	
 fi
 
 
 
+iptables_installed=1
 
+if [[ iptables_installed -ne 1 ]] ; then 
+
+    echo "$SCRIPT_NAME:$RUNTIME_DATE: iptables is not installed, iptables rules checking will be skipped"
+    exit
+fi
+
+
+#Checking iptables filter-table Policy:
+
+#INPUT chain  
+iptables -S | grep "\-P INPUT DROP" ; POLICY_STATUS=$?
+if [[ $POLICY_STATUS -ne 0 ]]; then 
+	echo "$SCRIPT_NAME : $RUNTIME_DATE : POLICY NOT MATCHED : The current iptables policy for the INPUT chain is ACCEPT but the recommended policy is DROP" >> $MESSAGES_FILE
+	echo "iptables -P INPUT DROP" >> $ACTIONS_FILE
+fi
+
+#OUTPUT chain: 
+iptables -S | grep "\-P OUTPUT DROP"  ; POLICY_STATUS=$? 
+if [[ $POLICY_STATUS -ne 0 ]]; then 
+	echo "$SCRIPT_NAME : $RUNTIME_DATE : POLICY NOT MATCHED : The current iptables policy for the OUTPUT chain is ACCEPT but the recommended policy is DROP"	>> $MESSAGES_FILE	 
+	echo "iptables -P OUTPUT DROP" >> $ACTIONS_FILE
+fi
+ 
+
+#FORWARD chain: 
+iptables -S | grep "\-P FORWARD DROP"  ; POLICY_STATUS=$? 
+if [[ $POLICY_STATUS -ne 0 ]]; then 
+	echo "$SCRIPT_NAME : $RUNTIME_DATE : POLICY NOT MATCHED : The current iptables policy for the FORWARD chain is ACCEPT but the recommended policy is DROP" >>	$MESSAGES_FILE
+	echo "iptables -P FORWARD DROP" >> $ACTIONS_FILE
+fi
+ 
+
+
+
+
+
+
+RULE_STATUS=0
 
 #Iterating over the keys of the iptables-rules (Rules Names): 
 for PARAM in $( echo "${!FW_Rules[@]}" | sed 's/[a-z\0-9\.\_\-]*,[d\1-9]//g' ); do
@@ -218,12 +284,16 @@ for PARAM in $( echo "${!FW_Rules[@]}" | sed 's/[a-z\0-9\.\_\-]*,[d\1-9]//g' ); 
     PARAM=${PARAM%,*}	        # Substring from the begging to the comma (,) to get the parameter name without the index
                             [[ $DEBUG -eq 1 ]] && echo "PARAM= ($PARAM)"
     check-pf ${PARAM} check     #Fetching the Used Acceptance for the current rule from the profile file.
-    USER_ACCEPTANCE=$?
+    USER_CHECK_ACCEPTENCE=$?
+	
+	check-pf ${PARAM} action
+	USER_ACTION_ACCEPTENCE=$?
 
-                            [[ $DEBUG -eq 1 ]] && echo "User Acceptance = $USER_ACCEPTANCE"
+                            [[ $DEBUG -eq 1 ]] && echo "User Acceptance = $USER_CHECK_ACCEPTENCE"
+							[[ $DEBUG -eq 1 ]] && echo "User Action Acceptance = $USER_ACTION_ACCEPTENCE"
 
     #Checking whether the rule is accepted, if it not just print a message and continue;                         
-    if [[ $USER_ACCEPTANCE == 0 ]] ; then echo "$0:$RUNTIME_DATE:($PARAM):RuleNotAccepted:Skipping." ; continue ; fi
+    if [[ $USER_CHECK_ACCEPTENCE == 0 ]] ; then echo "$SCRIPT_NAME:$RUNTIME_DATE:($PARAM):RuleNotAccepted:Skipping." ; continue ; fi
                            
                             [[ $DEBUG -eq 1  ]] && echo "Rule Accepted ($PARAM)"
 
@@ -234,21 +304,29 @@ for PARAM in $( echo "${!FW_Rules[@]}" | sed 's/[a-z\0-9\.\_\-]*,[d\1-9]//g' ); 
 
 
     #Adding the parameters to the rules before checking them. 
-    FINAL_RULE=$(ReplaceParameters $COUNTER $PARAM "${PARAMETERS_LIST}" )
+    FINAL_RULE=$( ReplaceParameters $COUNTER $PARAM "${PARAMETERS_LIST}" )
 
                             [[ $DEBUG -eq 1  ]] && echo -e "Finel Rule: ($FINAL_RULE)"
                             [[ $DEBUG -eq 1  ]] && echo -e "\nChecking Rule .. \n"
 
     #Pipping the rules to the check_rule function to be checked
-    echo -e $FINAL_RULE | check_rule 
-    RULE_STATUS=$?
+    if [[ $IPTABLES_NEW_INSTALLATION -ne 1 ]] ; then 
+		echo -e $FINAL_RULE | check_rule 
+    	RULE_STATUS=$?
 
-                            [[ $DEBUG -eq 1  ]] && echo "-----------------------------"
+ 	fi
+	                            [[ $DEBUG -eq 1  ]] && echo "-----------------------------"
 
     #Fetching the Description from the rules dictionary ;
     DESCRIPTION=${FW_Rules["$PARAM,d"]}
                             [[ $DEBUG -eq 1  ]] && echo "Description is ($DESCRIPTION)"
 
-    if [[ RULE_STATUS -ne 1 ]] ; then echo "$0:$RUNTIME_DATE:$PARAM:RULE_NOT:MATCHED:$DESCRIPTION" >> $MESSAGES_FILE ; fi
+    if [[ RULE_STATUS -ne 1 ]] ; then 
+
+							[[ $DEBUG -eq 1  ]] && echo "RULE NOT MATCHED"
+
+		echo -e "\n$SCRIPT_NAME : $RUNTIME_DATE : $PARAM : RULE NOT MATCHED : $DESCRIPTION" >> $MESSAGES_FILE 
+		[[ (USER_ACTION_ACCEPTENCE -eq 1)  && (GENERAL_ACTIONS_ACCEPTENCE -eq 1) ]] && echo -e $FINAL_RULE | write_action
+	fi
 done
 
