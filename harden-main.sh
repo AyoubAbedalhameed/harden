3184d69183f77b3d797b2e2bd1cbf3794f14af6d
nano9911 <aalkhaldi8@gmail.com>
2022-05-31 06:27:32 +0300

    added many options, and edited redirection modes

--- harden-main.sh
+++ harden-main.sh
@@ -5,7 +5,23 @@
 set -C
 
 _USAGE_FUNCTION() {
-	echo >&2 "Usage: $0 (scan - take-action - list-messages - list-actions - clear-all - rotate) -cf [configuration file] -pf [profile file] -d [date to list (messages or actions) in YYYY-MM-DD format]"
+	echo >&2 "Usage: $0 COMMAND [-c|-p|-d]"
+	printf >&2 '\nCOMMAND:\nAvailable commands are:\n'
+	printf >&2 '\tscan\t\t\t\tscan, depending on the profile file that will be used.\n'
+	printf >&2 '\ttake-action\t\t\trun the actions recommended from last scan, which was approved from the profile file "action" parameter.\n'
+	printf >&2 '\tlist-messages\t\t\tshow messages for a given date in a "less" like format (requires -d option).\n'
+	printf >&2 '\tclear-all\t\t\tdelete all remained data files (logs, messages, actions).\n'
+	printf >&2 '\trotate\t\t\t\tclear data files (logs, messages, actions) that are more than a month old.\n'
+	printf >&2 '\nOptional arguments:\n'
+	printf >&2 'Options used only with "scan" command:\n'
+	printf >&2 '\t-c, --config-file  FILE	\t\tuse FILE as the configuration file.\n'
+	printf >&2 '\t-p, --profile-file FILE	\t\tuse FILE as the profile file.\n'
+	printf >&2 '\nOptions used only with "list-messages" command:\n'
+	printf >&2 '\t-d, --date-to-list WORD	\t\tWORD represents date to list (messages or actions) in YYYY-MM-DD format.\n'
+	printf >&2 '\nOptions used with any command, but only for manually running the script (not as a service argument):\n'
+	printf >&2 '\t-o, --on-screen\t\t\twill only show service messages output on screen (if not used with -v), by default no STDERR or normal messages.\n'
+	printf >&2 '\t-v, --verbose\t\t\tshow all output (all messages and STDERR) without saving logs, will turn on -o by defaut. Note: not to be used as an argument in a service, it will be rejected.\n'
+	printf >&2 '\t--debug\t\t\t\trun with bash dubugging mode "-x". Note: not to be used as an argument in a service, it will be rejected.\n'
 }
 
 [[ $(id -u) != 0 ]] && {
@@ -18,29 +34,42 @@ RUNTIME_DATE=$(date '+%s_%F')	# Runtime date and time
 
 # First argument should specify which mode we are running in
 OPERATE_MODE=$1
-[[ $OPERATE_MODE =~ ^(scan)|(list-actions)|(list-messages)|(take-action)|(rotate)|(clear-all)$ ]] && {
+[[ ! $OPERATE_MODE =~ ^(scan)|(list-actions)|(list-messages)|(take-action)|(rotate)|(clear-all)$ ]] && {
 	_USAGE_FUNCTION
 	echo >&2 "$0: Please specify one of the available modes (scan - take-action - list-messages - list-actions - clear-all - rotate)"
 	exit 0
 }
 
-DEBUG_X=""
+__DEBUG_X=""
+__ON_SCREEN=0
+__ON_SCREEN_LOG=0
+
 shift
 # Loop through all command line arguments, and but them in
 # the case switch statement to test them
 while [[ $# -gt 0 ]]; do
 	case $1 in
-		-cf|--config-file)	# Use a configuration file from user choice
+		-c|--config-file)	# Use a configuration file from user choice
+			if [[ $OPERATE_MODE != "scan" ]]; then
+				echo >&2 "$0: invalid option, ($1) could be only used with (scan) command"
+				_USAGE_FUNCTION
+				exit 1
+			fi
 			CONFIG_FILE=$2
 			shift 2
 			;;
-		-pf|--profile-file)	# Use a profile file from user choice
+		-p|--profile-file)	# Use a profile file from user choice
+			if [[ $OPERATE_MODE != "scan" ]]; then
+				echo >&2 "$0: invalid option, ($1) could be only used with (scan) command"
+				_USAGE_FUNCTION
+				exit 1
+			fi
 			PROFILE_FILE="$2"
 			shift 2 
 			;;
 		-d|--date-to-list)	# This option is used in (list-messages, list-actions) operate modes
-			if [[ $OPERATE_MODE == scan ]]; then
-				echo >&2 "$0: invalid option, ($1) is only used with (list-messages, list-actions) modes"
+			if [[ $OPERATE_MODE != "list-messages" ]]; then
+				echo >&2 "$0: invalid option, ($1) is only used with (list-messages) command"
 				_USAGE_FUNCTION
 				exit 1
 			elif [[ ! $1 =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
@@ -51,8 +80,32 @@ while [[ $# -gt 0 ]]; do
 			DATE_TO_LIST=$2
 			shift 2
 			;;
-		-x)
-			DEBUG_X="-x"
+		-o|--on-screen)
+			[[ $__LAUNCHED_BY_SYSTEMD == 1 ]] && {
+				echo "$0: Error, harden.service unit file shouldnot give the '-o' option for main in the 'ExecStart' variable, bye."
+				_USAGE_FUNCTION
+				exit 1
+			}
+			__ON_SCREEN=1
+			shift 1
+		;;
+		-v|--verbose)
+			[[ $__LAUNCHED_BY_SYSTEMD == 1 ]] && {
+				echo "$0: Error, harden.service unit file shouldnot give the '-v' option for main in the 'ExecStart' variable, bye."
+				_USAGE_FUNCTION
+				exit 1
+			}
+			__ON_SCREEN=1
+			__ON_SCREEN_LOG=1
+			shift 1
+		;;
+		--debug)
+			[[ $__LAUNCHED_BY_SYSTEMD == 1 ]] && {
+				echo "$0: Error, harden.service unit file shouldnot give the '-x' option for main in the 'ExecStart' variable, exit."
+				_USAGE_FUNCTION
+				exit 1
+			}
+			__DEBUG_X="-x"
 			set -x
 			shift 1
 			;;
@@ -63,7 +116,7 @@ while [[ $# -gt 0 ]]; do
 			;;
 		*)
 			POSITIONAL_ARGS+=("$1")	# save positional arguments
-			shift
+			shift 1
 			;;
 		esac
 done
@@ -82,16 +135,19 @@ CONFIG_DIR="/etc/harden"	# Default Configuration Directory
 CONFIG_FILE=${CONFIG_FILE:="$CONFIG_DIR/harden.conf"}	# Use Default Configuration File,
 									# if not set by a positional parameter (command line argument)
 if [[ -n $PROFILE_FILE ]] && [[ ! -e $PROFILE_FILE ]]; then
-	echo >&2 "$0: Invalid data for -pf (profile file): $PROFILE_FILE no such file."
+	echo >&2 "$0: Invalid data for -p (profile file): $PROFILE_FILE no such file."
 	_USAGE_FUNCTION
 	exit 1
+
 elif [[ -z $PROFILE_FILE ]] && [[ -e $CONFIG_DIR/profile-file.json ]]; then
 	PROFILE_FILE="$CONFIG_DIR/profile-file.json"	# Use Default User Choice Profile File,
+
 elif [[ -z $PROFILE_FILE ]] && [[ -e $MAIN_DIR/config/profile-file.json ]]; then
 	PROFILE_FILE="$MAIN_DIR/config/profile-file.json"	# if not set by a positional parameter (command line argument)
+
 elif [[ -z $PROFILE_FILE ]]; then
 	echo >&2 "$0: Critical Error: JSON file \"profile-file.json\" which is the main congifuration file for the Linux Hardening Service, is missing. \
-Couldn't find it in: /etc/harden/profile-file.json, or /usr/share/harden/config/profile-file.json"
+Couldn't find it in: $CONFIG_DIR, or $MAIN_DIR/config"
 	exit 1
 fi
 
@@ -105,7 +161,7 @@ mkdir -p $MESSAGES_DIR
 mkdir -p $ACTIONS_DIR
 
 MESSAGES_FILE="$MESSAGES_DIR/harden-messages_$RUNTIME_DATE"	# Currently used messages file
-ACTIONS_FILE="$ACTIONS_DIR/harden-action_$RUNTIME_DATE.sh"	# Currently used Actions file
+ACTIONS_FILE="$ACTIONS_DIR/harden-recommended-action"	# Currently used Actions file
 
 # Redirect stderr to the log file, so everything
 # will be recorded in it. Also, due to the tail command on the
@@ -131,7 +187,8 @@ if [[ $__LAUNCHED_BY_SYSTEMD == 1 ]]; then	# Only add Syslog Identifier prefix <
 	exec 5> >(while read -r __NOTICE; do echo >&6 "<5>$__NOTICE"; done)
 	trap 'exec >&5-' EXIT
 
-elif [[ -z $DEBUG_X ]]; then
+# If main was not ran by systemd, and on screen log output variables is empty, we will need to write, redirect, format and save our logs manually in a log file
+elif [[ $__ON_SCREEN_LOG == 0 ]]; then
 	# Write every thing to a log file, in a formatted way with full usefull info
 	LOGS_DIR="/var/log/harden"	# Default Actions Directory
 	mkdir -p $LOGS_DIR	# Check if Directory exists, and if not then create it
@@ -141,16 +198,25 @@ elif [[ -z $DEBUG_X ]]; then
 	touch "$LOG_FILE"
 
 	# Default STDOUT to be prefixed with syslog identifier <6> for INFO, with more logging information then redirect to log file
-	exec 1> >(while read -r __INFO; do echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <info> [$(date %s.%^4N)] $__INFO" >> "$LOG_FILE" ; done)
+	exec 1> >(while read -r __INFO; do [[ -n $__INFO ]] && echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <info> [$(date %s.%^4N)] $__INFO" >> "$LOG_FILE" ; done)
 	trap 'exec >&1-' EXIT
 
 	# Default STDERR to be prefixed with syslog identifier <7> for DEBUG, with more logging information then redirect to log file
-	exec 2> >(while read -r __LOG_DEBUG; do echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <debug> [$(date %s.%^4N)] $__LOG_DEBUG" >> "$LOG_FILE"; done)
+	exec 2> >(while read -r __DEBUG; do [[ -n $__DEBUG ]] && echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <debug> [$(date %s.%^4N)] $__DEBUG" >> "$LOG_FILE"; done)
 	trap 'exec >&2-' EXIT
 
-	# Default messages to be prefixed with syslog identifier <5> for NOTICE, with more logging information then redirect to log file
-	exec 5> >(while read -r __NOTICE; do echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <notice> [$(date %s.%^4N)] $__NOTICE" >> "$LOG_FILE"; done)
-	trap 'exec >&5-' EXIT
+	if [[ $__ON_SCREEN == 1 ]]; then
+		exec 5>&1	# in case of on screen mode, we are just creating a clone fd of STDOUT to ptint messages as is on screen
+		trap "exec >&5-" EXIT
+	else	# Default messages to be prefixed with syslog identifier <5> for NOTICE, with more logging information then redirect to log file
+		exec 5> >(while read -r __NOTICE; do [[ -n $__NOTICE ]] && echo "$(date '+%F %T') $HOSTNAME harden-service[$$]: <notice> [$(date %s.%^4N)] $__NOTICE" >> "$LOG_FILE"; done)
+		trap 'exec >&5-' EXIT
+	fi
+
+# in case of on screen mode, we are just creating a clone fd of STDOUT to shorten our code in the next lines of _HARDEN_RUN_FUNCTION()
+else
+	exec 5>&1
+	trap "exec >&5-" EXIT
 fi
 
 ########################################################
@@ -194,7 +260,7 @@ _HARDEN_RUN_FUNCTION()   {
 				echo >&2 "$0: Script $script does not exist in the scripts directory $SCRIPTS_DIR. Skipping $script, due to it's suspecious location."
 				continue
 			fi
-			bash "$DEBUG_X" "$script" -mf "$MESSAGES_FILE" -af "$ACTIONS_FILE" -pf "$PROFILE_FILE"
+			bash "$__DEBUG_X" "$script" -mf "$MESSAGES_FILE" -af "$ACTIONS_FILE" -p "$PROFILE_FILE"
 		else
 			echo >&2 "$0: Script $script does not exist. Please, check what is wrong either in the profile-file.json, or if there's any missing package files."
 		fi
