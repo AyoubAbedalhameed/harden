d71539e63a32e3eb3b305fdc0621827e69d296ae
nano9911 <aalkhaldi8@gmail.com>
2022-06-10 19:29:46 +0300

    fixed writing action file mechanism

--- scripts/kernel-harden.sh
+++ scripts/kernel-harden.sh
@@ -22,6 +22,7 @@ LOG_FILE=$LOG_FILE"
 
 declare -r STATUS_FILE="$STATUS_DIR/kernel-harden.status"	# Currently used status file
 declare -r KERNEL_ACTIONS_FILE="$ACTIONS_DIR/kernel-actions.sh"
+echo -n "" > "$KERNEL_ACTIONS_FILE"
 
 echo ""
 echo "Kernel Hardening script has started..."
@@ -41,6 +42,7 @@ fi
 _check_param_function()	{
 	source "$PARAMETERS_FILE"
 
+	declare -r SYSCTL_FILE="/usr/lib/sysctl.d/99-harden-syctl.conf"
 	# Declare these variables with integer and readonly attributes to prevent mistakes
 	declare -ri VAL_INDEX=0 MES_INDEX=1 TYPE_INDEX=2
 
@@ -67,7 +69,18 @@ _check_param_function()	{
 		P=${P//-/_}
 		echo "kernel_parameter_$P=\"$CURRENT_VAL\"" >> "$STATUS_FILE"	# Save the current value
 
-		[[ $(_check_profile_file_function kernel "$TYPE" action) == 1 ]]  && echo "sysctl -w $PARAM $RECOMMENDED_VAL" >> "$KERNEL_ACTIONS_FILE"	# Save action
+		[[ $(_check_profile_file_function kernel "$TYPE" action) == 1 ]]  &&	{
+			{
+				echo "sysctl -w $PARAM $RECOMMENDED_VAL"
+				echo "{"
+				echo "echo \"# This was written when $PARAM current value was $CURRENT_VAL\""
+				echo "echo \"# $PARAM recommended value is $RECOMMENDED_VAL\""
+				echo "echo \"# $MESSAGE\""
+				echo "echo \"$PARAM = $RECOMMENDED_VAL\""
+				echo "} >> $SYSCTL_FILE"
+				echo ""
+			} >> "$KERNEL_ACTIONS_FILE"
+		}
 	done
 }
 
@@ -79,18 +92,27 @@ _check_module_blacklisting_function()	{
 
 	if [[ ! -f $MODULE_BLACKLIST_FILE ]]; then
 		[[ $(_check_profile_file_function kernel action) == 1 ]] && echo "touch $MODULE_BLACKLIST_FILE" >> "$KERNEL_ACTIONS_FILE"
-		echo "Kernel-Module-Hardening: Your system doesn't have any modules blocked in $MODULE_BLACKLIST_FILE (it doesn't even exist)" >> $MESSAGES_FILE
+		echo "Kernel-Module-Hardening: Your system doesn't have any modules blocked in $MODULE_BLACKLIST_FILE (it doesn't even exist)" >> "$MESSAGES_FILE"
 
 		for TYPE in $MOD_TYPES; do
 			if [[ $(_check_profile_file_function kernel module "$TYPE" check) == 1 ]]
 			then
 				for MODULE in ${!TYPE}; do
 					[[ ! $RUNNING_MODULES =~ (^|[[:space:]])"$MODULE"($|[[:space:]]) ]] && continue
+
 					echo "kernel_module_$MODULE=1" >> "$STATUS_FILE"
 					echo "Kernel-Module-Hardening[$MODULE]: Kernel module $MODULE currently loaded and running on your system, it is recommended to be blacklisted, \
 because either it has a history of vulnerabilities, or it's weak." >> "$MESSAGES_FILE"
 
-					[[ $(_check_profile_file_function kernel module "$TYPE" action) == 1 ]] && echo "echo \"blacklist $MODULE\" >> $MODULE_BLACKLIST_FILE" >> "$ACTIONS_FILE"
+					[[ $(_check_profile_file_function kernel module "$TYPE" action) == 1 ]] && {
+						{
+							echo "echo \"# $MODULE is not recommended to be loaded or used, to keep the system secure and hardened\" >> $MODULE_BLACKLIST_FILE"
+							echo "echo \"blacklist $MODULE\" >> $MODULE_BLACKLIST_FILE"
+							echo ""
+							echo "modprobe -r $MODULE"	# Save the action of unloading the module
+							echo ""
+					 	} >> "$KERNEL_ACTIONS_FILE"
+					}
 				done
 			fi
 		done
@@ -100,8 +122,11 @@ because either it has a history of vulnerabilities, or it's weak." >> "$MESSAGES
 			if [[ $(_check_profile_file_function kernel module "$TYPE" check) == 1 ]]
 			then
 				for MODULE in ${!TYPE}; do
-					[[ $RUNNING_MODULES =~ (^|[[:space:]])"$MODULE"($|[[:space:]]) ]] && echo "Kernel-Hardening[$MODULE]: Warning!! Kernel module $MODULE is loaded on you currently \
+					[[ $RUNNING_MODULES =~ (^|[[:space:]])"$MODULE"($|[[:space:]]) ]] && {
+						echo "modprobe -r $MODULE" >> "$KERNEL_ACTIONS_FILE"
+						echo "Kernel-Hardening[$MODULE]: Warning!! Kernel module $MODULE is loaded on you currently \
 running system, but it's dangerous for security reasons." >> "$MESSAGES_FILE"
+					}
 
 					grep -q "$MODULE" "$MODULE_BLACKLIST_FILE" && continue
 
@@ -109,17 +134,24 @@ running system, but it's dangerous for security reasons." >> "$MESSAGES_FILE"
 					echo "Kernel-Module-Hardening[$MODULE]: Kernel module $MODULE is recommended to be blacklisted, because either it has a history of vulnerabilities, \
 or it's weak." >> "$MESSAGES_FILE"
 
-					[[ $(_check_profile_file_function kernel module "$TYPE" action) == 1 ]] && echo "echo \"blacklist $MODULE\" >> $MODULE_BLACKLIST_FILE" >> "$ACTIONS_FILE"
+					[[ $(_check_profile_file_function kernel module "$TYPE" action) == 1 ]] && echo "echo \"blacklist $MODULE\" >> $MODULE_BLACKLIST_FILE" >> "$KERNEL_ACTIONS_FILE"
 				done
 			fi
 		done
 	fi
+	[[ $(_check_profile_file_function kernel module action) == 1 ]] && {
+		echo "# run 'mkinitrd' to generate a new initrd image" >> "$KERNEL_ACTIONS_FILE"
+		echo "mkinitrd" >> "$KERNEL_ACTIONS_FILE"
+	}
 }
+
+# Prepare kernel actions file to be identified as a script
+[[ $(_check_profile_file_function kernel action) == 1 ]] && printf '#!/usr/bin/env bash\n\n' >> "$KERNEL_ACTIONS_FILE"
 
 [[ $(_check_profile_file_function kernel check) == 1 ]] && _check_param_function
 [[ $(_check_profile_file_function kernel module check) == 1 ]] && _check_module_blacklisting_function
 
-[[ $(_check_profile_file_function kernel action) == 1 ]] && echo "$KERNEL_ACTIONS_FILE" >> "$ACTIONS_FILE"	# Add approved actions to the actions file
+[[ $(_check_profile_file_function kernel action) == 1 ]] && echo "bash $KERNEL_ACTIONS_FILE" >> "$ACTIONS_FILE"	# Add approved actions to the actions file
 
 echo ""
 echo "Kernel Hardening script has finished"
