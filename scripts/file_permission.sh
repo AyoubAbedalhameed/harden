2d151c236f5937507d9d94dd4a3c700a0a0d0684
Adnan Omar <aalkhaldi8@gmail.com>
2022-06-25 23:48:45 +0000

    Update file_permission.sh

--- scripts/file_permission.sh
+++ scripts/file_permission.sh
@@ -20,11 +20,7 @@ MESSAGES_FILE = $MESSAGES_FILE
 ACTIONS_FILE = $ACTIONS_FILE
 LOG_FILE=$LOG_FILE"
 
-
 [[ $(_check_profile_file_function file_permission check) == 0 ]] && exit
-
-
-
 
 PARAMETER_FILE="$MAIN_DIR/resources/rolefile.txt"
 echo "check permission start ..."
@@ -37,7 +33,6 @@ while read -r line; do
 	group=$(echo $line | awk '{print $5;}')
 	file=$(echo $line | awk '{print $6;}')
 	message=${line#*=+@}
-#	message=$(echo $line |awk '{for (i=6;i<NF;i++) print $i " "; print $NF}')
 
 	[[ $(_check_profile_file_function file_permission permissions check) != 1 ]] && find $file -perm $perm && {
 		echo "File_Permission permissions -[$service]:'$file' file permissions user/owner group is not as recommended ($perm). $message" >> "$MESSAGES_FILE"
